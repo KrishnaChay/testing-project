8fe62cce9225064bc8f8fda4178b60965a44e13a
Chaitanya <Krishna.chaitanya@directlinegroup.co.uk>
2019-11-18 21:42:08 +0000

    intial commit

---
+++ src/test/java/com/chay/testing/step_definitions/DemoOne.java
@@ -0,0 +1,60 @@
+package com.chay.testing.step_definitions;
+
+import com.chay.testing.interactors.DemoPageInteractor;
+import com.usmanhussain.habanero.context.TestContext;
+import com.usmanhussain.habanero.framework.StepDefs;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class DemoOne extends StepDefs {
+
+    private DemoPageInteractor demoPageInteractor;
+
+    public DemoOne(TestContext context) {
+        super(context);
+    }
+
+    @Given("^one$")
+    public void one() throws Throwable {
+        getContext().getDriver().get("https://httpstat.us/200");
+        getContext().getDriver().get("https://httpstat.us/404");
+    }
+
+    @When("^two$")
+    public void two() throws Throwable {
+        WebElement element = getContext().getDriver().findElement(By.id("lst-ib"));
+        element.sendKeys("Hi" + Keys.ENTER);
+        getContext().getDriver().findElement(By.cssSelector("a[href='http://www.dictionary.com/browse/hi']")).click();
+    }
+
+    @Then("^three$")
+    public void three() throws Throwable {
+    }
+
+    @Given("^I launch evo application$")
+    public void iLaunchASlowApplication() throws Throwable {
+        getContext().getDriver().get("https://dlg-sit2.ssp-hosting.com/DirectLineVanCT/servletcontroller");
+    }
+
+    @And("^I click next$")
+    public void iClickNext() throws Throwable {
+        Thread.sleep(5000);
+        getContext().getDriver().findElement(By.id("C2__BUT_9D492D0AD73E6FD4361634")).click();
+    }
+
+    @Then("^I should see registration text field$")
+    public void iShouldSeeRegistrationTextField() throws Throwable {
+        getContext().getDriver().findElement(By.id("C2__C1__QUE_D60ACA93B3AF52B1128920")).sendKeys("A1");
+    }
+
+    @And("^I wait$")
+    public void iWait() throws Throwable {
+        Thread.sleep(10000);
+    }
+
+}
---
+++ src/test/java/com/chay/testing/step_definitions/DemoTwo.java
@@ -0,0 +1,19 @@
+package com.chay.testing.step_definitions;
+
+import com.usmanhussain.habanero.context.TestContext;
+import com.usmanhussain.habanero.framework.StepDefs;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+
+public class DemoTwo extends StepDefs {
+
+    public DemoTwo(TestContext context) {
+        super(context);
+    }
+
+    @Then("^four$")
+    public void four() throws Throwable {
+        throw new PendingException();
+    }
+
+}
